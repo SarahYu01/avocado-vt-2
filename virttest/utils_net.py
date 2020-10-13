716ac1eab125b516aa0c739b8a31c78d3009d50d
yushuang <yushuang@loongson.cn>
2020-10-13 15:46:41 +0800

    utils_net.py: update sys_ifname param

--- virttest/utils_net.py
+++ virttest/utils_net.py
@@ -3666,7 +3666,7 @@ def get_linux_ifname(session, mac_address=""):
             interface name.
     """
     def _process_output(cmd, reg_pattern):
-        sys_ifname = ["lo", "sit0", "virbr0"]
+        sys_ifname = ["lo", "sit0", "virbr0", "dummy0"]
         try:
             output = session.cmd_output_safe(cmd)
             ifname_list = re.findall(reg_pattern, output, re.I)
